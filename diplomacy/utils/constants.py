9399fb7160d748640058b5cdb6e1984f887fa792
Matteo Bortolotto <mbortolotto@fbk.eu>
2025-05-24 14:54:00 +0200

    Change host for render

--- diplomacy/utils/constants.py
+++ diplomacy/utils/constants.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU Affero General Public License along
 #  with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ==============================================================================
-""" Some constant / config values used in Diplomacy package. """
+"""Some constant / config values used in Diplomacy package."""
 
 # Number of times to try to connect before throwing an exception.
 NB_CONNECTION_ATTEMPTS = 12
@@ -32,7 +32,7 @@ DEFAULT_PING_SECONDS = 30
 REQUEST_TIMEOUT_SECONDS = 30
 
 # Default host name for a server to connect to.
-DEFAULT_HOST = 'localhost'
+DEFAULT_HOST = "0.0.0.0"
 
 # Default port for normal non-securized server.
 DEFAULT_PORT = 8432
@@ -42,18 +42,20 @@ DEFAULT_SSL_PORT = 8433
 
 # Special username and password to use to connect as a bot recognized by diplomacy module.
 # This bot is called "private bot".
-PRIVATE_BOT_USERNAME = '#bot@2e723r43tr70fh2239-qf3947-3449-21128-9dh1321d12dm13d83820d28-9dm,xw201=ed283994f4n832483'
-PRIVATE_BOT_PASSWORD = '#bot:password:28131821--mx1fh5g7hg5gg5g´[],s222222223djdjje399333x93901deedd|e[[[]{{|@S{@244f'
+PRIVATE_BOT_USERNAME = "#bot@2e723r43tr70fh2239-qf3947-3449-21128-9dh1321d12dm13d83820d28-9dm,xw201=ed283994f4n832483"
+PRIVATE_BOT_PASSWORD = "#bot:password:28131821--mx1fh5g7hg5gg5g´[],s222222223djdjje399333x93901deedd|e[[[]{{|@S{@244f"
 
 # Time to wait to let a bot set orders for a dummy power.
 PRIVATE_BOT_TIMEOUT_SECONDS = 60
 
 # Default rules used to construct a Game object when no rules are provided.
-DEFAULT_GAME_RULES = ('SOLITAIRE', 'NO_PRESS', 'IGNORE_ERRORS', 'POWER_CHOICE')
+DEFAULT_GAME_RULES = ("SOLITAIRE", "NO_PRESS", "IGNORE_ERRORS", "POWER_CHOICE")
+
 
 class OrderSettings:
-    """ Constants to define flags for attribute Power.order_is_set. """
-    #pylint:disable=too-few-public-methods
+    """Constants to define flags for attribute Power.order_is_set."""
+
+    # pylint:disable=too-few-public-methods
     ORDER_NOT_SET = 0
     ORDER_SET_EMPTY = 1
     ORDER_SET = 2
